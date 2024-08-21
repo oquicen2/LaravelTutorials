c9055a487f9ff315f397b5e97695a0f89d742739
Orleis Quiceno <oquicen2@gmail.com>
2024-08-21 13:43:20 -0500

    Tutorial2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Product;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        Product::factory(8)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -3,23 +3,23 @@
 @section('subtitle', $viewData["subtitle"])
 @section('content')
 <div class="card mb-3">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="https://laravel.com/img/logotype.min.svg" class="img-fluid rounded-start">
-        </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title">
-                    @if ($viewData["product"]["price"] > 100)
-                    <span style="color: red;">{{ $viewData["product"]["name"] }}</span>
-                    @else
-                        {{ $viewData["product"]["name"] }}
-                    @endif
-                </h5>
-                <p class="card-text">{{ $viewData["product"]["description"] }}</p>
-                <p class="card-text text-red">{{ $viewData["product"]["price"] }}</p>
-            </div>
-        </div>
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://laravel.com/img/logotype.min.svg" class="img-fluid rounded-start">
     </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">
+           {{ $viewData["product"]["name"] }}
+        </h5>
+        <p class="card-text">{{ $viewData["product"]["price"] }}</p>
+
+        @foreach($viewData["product"]->comments as $comment)
+          - {{ $comment->getDescription() }}<br />
+        @endforeach
+
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
